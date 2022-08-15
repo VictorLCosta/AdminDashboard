8ef05964e1db518eef30c8573a53a274ea86ed59
VictorLCosta <victorlc2019@outlook.com>
2022-08-15 20:00:51 -0300

    Home part 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ var header = document.querySelector(".header")
 menuBtn.onclick = () => {
     header.classList.toggle("active")
 }
+
+var cardMenuBtn = document.querySelector("[data-menu-btn]")
+var ctxMenu = document.querySelector("[data-ctx-menu]")
+
+cardMenuBtn.onclick = () => {
+    ctxMenu.classList.toggle("active")
+}
